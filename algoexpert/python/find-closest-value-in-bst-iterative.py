29b97d64ee62378d4963fc1c61165940ec0a9220
decaplanet <126875757+decaplanet@users.noreply.github.com>
2023-06-28 17:01:02 +0900

    Add New Solution

---
+++ algoexpert/python/find-closest-value-in-bst-iterative.py
@@ -0,0 +1,31 @@
+# https://www.algoexpert.io/questions/find-closest-value-in-bst
+# Answer from Code Walkthrough
+
+def findClosestValueInBst(tree, target):
+    return find_closest_value_in_bst_helper(tree, target, float("inf"))
+
+
+def find_closest_value_in_bst_helper(tree, target, closest):
+
+    current_node = tree
+
+    while current_node != None:
+        if abs(target - closest) > abs(target - current_node.value):
+            closest = current_node.value
+
+        if target < current_node.value:
+            current_node = current_node.left
+        elif target > current_node.value:
+            current_node = current_node.right
+        else:
+            break
+
+    return closest
+
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
