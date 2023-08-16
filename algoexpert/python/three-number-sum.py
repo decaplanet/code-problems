8109b68e3c357d0d9ad9b59ed0103ab66d9ab062
decaplanet <126875757+decaplanet@users.noreply.github.com>
2023-08-16 15:51:51 +0900

    Add New Solution

---
+++ algoexpert/python/three-number-sum.py
@@ -0,0 +1,28 @@
+# https://www.algoexpert.io/questions/three-number-sum
+# Answer Provided by AlgoExpert
+
+
+# O(n^2) time | O(n) space
+def threeNumberSum(array: list[int], targetSum: int):
+    array.sort()
+    # array = sorted(array)
+
+    triplets = []
+
+    for i in range(len(array) - 2):
+        left = i + 1
+        right = len(array) - 1
+
+        while left < right:
+            currentSum = array[i] + array[left] + array[right]
+
+            if currentSum == targetSum:
+                triplets.append([array[i], array[left], array[right]])
+                left += 1
+                right -= 1
+            elif currentSum < targetSum:
+                left += 1
+            elif currentSum > targetSum:
+                right -= 1
+
+    return triplets
