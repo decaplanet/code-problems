21f020bdb1f2ea5a74d7811f7d3983e3cba84e02
decaplanet <126875757+decaplanet@users.noreply.github.com>
2023-08-21 22:34:06 +0900

    Use Structs instead of Arrays

--- algoexpert/rust/move-element-to-end.rs
+++ algoexpert/rust/move-element-to-end.rs
@@ -1,34 +1,74 @@
-const TEST_CASES: [(&[i32], i32, &[i32]); 11] = [
-    (&[2, 1, 2, 2, 2, 3, 4, 2], 2, &[4, 1, 3, 2, 2, 2, 2, 2]),
-    (&[], 3, &[]),
-    (&[1, 2, 4, 5, 6], 3, &[1, 2, 4, 5, 6]),
-    (&[3, 3, 3, 3, 3], 3, &[3, 3, 3, 3, 3]),
-    (&[3, 1, 2, 4, 5], 3, &[5, 1, 2, 4, 3]),
-    (&[1, 2, 4, 5, 3], 3, &[1, 2, 4, 5, 3]),
-    (&[1, 2, 3, 4, 5], 3, &[1, 2, 5, 4, 3]),
-    (&[2, 4, 2, 5, 6, 2, 2], 2, &[6, 4, 5, 2, 2, 2, 2]),
-    (
-        &[5, 5, 5, 5, 5, 5, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12],
-        5,
-        &[12, 11, 10, 9, 8, 7, 1, 2, 3, 4, 6, 5, 5, 5, 5, 5, 5],
-    ),
-    (
-        &[1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 5, 5, 5, 5, 5, 5],
-        5,
-        &[1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 5, 5, 5, 5, 5, 5],
-    ),
-    (
-        &[5, 1, 2, 5, 5, 3, 4, 6, 7, 5, 8, 9, 10, 11, 5, 5, 12],
-        5,
-        &[12, 1, 2, 11, 10, 3, 4, 6, 7, 9, 8, 5, 5, 5, 5, 5, 5],
-    ),
+// https://www.algoexpert.io/questions/move-element-to-end
+
+struct TestCase<'a, 'b> {
+    array: &'a [i32],
+    to_move: i32,
+    answer: &'b [i32],
+}
+
+const TEST_CASES: [TestCase; 11] = [
+    TestCase {
+        array: &[2, 1, 2, 2, 2, 3, 4, 2],
+        to_move: 2,
+        answer: &[4, 1, 3, 2, 2, 2, 2, 2],
+    },
+    TestCase {
+        array: &[],
+        to_move: 3,
+        answer: &[],
+    },
+    TestCase {
+        array: &[1, 2, 4, 5, 6],
+        to_move: 3,
+        answer: &[1, 2, 4, 5, 6],
+    },
+    TestCase {
+        array: &[3, 3, 3, 3, 3],
+        to_move: 3,
+        answer: &[3, 3, 3, 3, 3],
+    },
+    TestCase {
+        array: &[3, 1, 2, 4, 5],
+        to_move: 3,
+        answer: &[5, 1, 2, 4, 3],
+    },
+    TestCase {
+        array: &[1, 2, 4, 5, 3],
+        to_move: 3,
+        answer: &[1, 2, 4, 5, 3],
+    },
+    TestCase {
+        array: &[1, 2, 3, 4, 5],
+        to_move: 3,
+        answer: &[1, 2, 5, 4, 3],
+    },
+    TestCase {
+        array: &[2, 4, 2, 5, 6, 2, 2],
+        to_move: 2,
+        answer: &[6, 4, 5, 2, 2, 2, 2],
+    },
+    TestCase {
+        array: &[5, 5, 5, 5, 5, 5, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12],
+        to_move: 5,
+        answer: &[12, 11, 10, 9, 8, 7, 1, 2, 3, 4, 6, 5, 5, 5, 5, 5, 5],
+    },
+    TestCase {
+        array: &[1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 5, 5, 5, 5, 5, 5],
+        to_move: 5,
+        answer: &[1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 5, 5, 5, 5, 5, 5],
+    },
+    TestCase {
+        array: &[5, 1, 2, 5, 5, 3, 4, 6, 7, 5, 8, 9, 10, 11, 5, 5, 12],
+        to_move: 5,
+        answer: &[12, 1, 2, 11, 10, 3, 4, 6, 7, 9, 8, 5, 5, 5, 5, 5, 5],
+    },
 ];
 
 fn main() {
-    for (array, to_move, answer) in TEST_CASES {
-        let mut array = array.to_vec();
-        let result = move_element_to_end(&mut array, to_move);
-        assert_eq!(result, answer);
+    for item in TEST_CASES {
+        let mut array = item.array.to_vec();
+        let result = move_element_to_end(&mut array, item.to_move);
+        assert_eq!(result, item.answer);
     }
 
     println!("Yay, your code passed all the test cases!");
