4551244c949e37bace27918a5913cd2ea6c0a92e
decaplanet <deca@decaplanet.com>
2023-12-13 09:34:42 +0900

    Format code indents

--- leetcode/swift/fizz-buzz.swift
+++ leetcode/swift/fizz-buzz.swift
@@ -3,13 +3,13 @@
 class Solution {
     func fizzBuzz(_ n: Int) -> [String] {
         var answer: [String] = []
-        
+
         for i in 1...n {
             if i % 3 != 0 && i % 5 != 0 {
                 answer.append("\(i)")
                 continue
             }
-            
+
             var appendingMsg = ""
             if i % 3 == 0 {
                 appendingMsg.append("Fizz")
@@ -17,10 +17,10 @@ class Solution {
             if i % 5 == 0 {
                 appendingMsg.append("Buzz")
             }
-            
+
             answer.append(appendingMsg)
         }
-        
+
         return answer
     }
 }
